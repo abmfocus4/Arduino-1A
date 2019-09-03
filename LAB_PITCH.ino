a86c01f4c6e0324f61778bd0c6f9179ce7020452
Meg Alapati <bmalapat@edu.uwaterloo.ca>
2019-09-02 20:51:01 -0400

    Add files via upload

---
+++ LAB_PITCH.ino
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int PWM;
+String l;
+void setup() {
+  
+  //A
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT); 
+
+  //B
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Enter the PWM value for two motors.");
+}
+
+void loop(){
+  if (Serial.available()>0){
+  l=Serial.readString();
+  PWM=l.toInt();
+  
+  Serial.println(l);
+  //A
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  analogWrite(3, PWM);
+
+  //B
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH); 
+  analogWrite(11, PWM); 
+
+  }
+
+}
---
+++ sketch_nov29a.ino
@@ -0,0 +1,32 @@
+
+int pot2=A1;
+const int motor_IN1=11; // MOTOR input 1(L293)
+const int motor_IN2=4; // motor input 2(L293)
+const int motor_ENA=9; //enable pin(L293) 
+double speed_Level; //variable of speed level
+int Percent;
+void setup() {
+  Serial.begin(9600);
+ pinMode(motor_IN1,OUTPUT);
+ pinMode(motor_IN2,OUTPUT);
+pinMode(11,OUTPUT);
+TCCR2B=(TCCR2B&0xF8) | 2;
+
+}
+
+void loop() {
+ analogWrite(motor_IN1,speed_Level); 
+ analogWrite(motor_IN2,LOW);
+
+speed_Level=analogRead(pot2);  
+speed_Level=constrain(speed_Level,0,255);
+
+Percent =(speed_Level/255)*100;
+
+Serial.print("PWM LEVEL= ");
+Serial.print(speed_Level);
+Serial.print("  , ");
+Serial.print(" speed level in Percent= %");
+Serial.print(Percent);
+Serial.print("\n");
+}
